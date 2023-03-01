cbc033d92931d7c6e021af780deb279dfd4cd035
Jon Seager <jon@sgrs.uk>
2023-03-01 12:34:39 +0000

    fix: stop trying to use error wrapping in a call to log

--- internal/releases/report.go
+++ internal/releases/report.go
@@ -27,7 +27,7 @@ func GenerateReport(conf *config.Config) ReleaseReport {
 			defer wg.Done()
 			err := team.Process()
 			if err != nil {
-				log.Printf("error processing team '%s': %w", team.Info().Name, err)
+				log.Printf("error processing team '%s': %v", team.Info().Name, err)
 			}
 		}()
 	}
