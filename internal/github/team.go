7ff283f407a8ece9dc69e29e28f660e15d493112
Jon Seager <jon.seager@canonical.com>
2023-11-20 18:12:49 +0000

    refactor: simplify github org parsing for readability

--- internal/github/team.go
+++ internal/github/team.go
@@ -15,62 +15,74 @@ const githubPerPage = 1000
 // FetchOrgRepos creates a slice of RepoDetails types representing the repos
 // owned by the specified teams in the Github org.
 func FetchOrgRepos(org OrgConfig) ([]repos.RepoDetails, error) {
-	ctx := context.Background()
-	opts := &gh.ListOptions{PerPage: githubPerPage}
-	out := []repos.RepoDetails{}
+	orgRepos := []repos.RepoDetails{}
 
 	// Iterate over the Github Teams, listing repos for each.
 	for _, team := range org.Teams {
-		// Lists the Github repositories that the 'ghTeam' has access to.
-		orgRepos, _, err := org.GithubClient().Teams.ListTeamReposBySlug(ctx, org.Org, team, opts)
+		// Process the Github orgs repositories into structs with the info we need.
+		teamRepos, err := getTeamRepos(org, team, orgRepos)
 		if err != nil {
-			return nil, fmt.Errorf("error listing repositories for github org: %s", org.Org)
-		}
-
-		ghRepos := []*Repository{}
-
-		// Iterate over repositories, populating release info for each.
-		for _, orgRepo := range orgRepos {
-			r := orgRepo
-			// Check if the name of the repository is in the ignore list or private.
-			if slices.Contains(org.IgnoredRepos, *r.Name) || *r.Private {
-				continue
-			}
-
-			// See if we can find a repo in this team with the same name, if the repository has
-			// already been added, skip.
-			index := slices.IndexFunc(out, func(repo repos.RepoDetails) bool {
-				return repo.Name == *r.Name
-			})
-			if index >= 0 {
-				continue
-			}
-
-			repo := &Repository{
-				Details: repos.RepoDetails{
-					Name:          *r.Name,
-					DefaultBranch: *r.DefaultBranch,
-					URL:           *r.HTMLURL,
-				},
-				org:    org.Org,
-				team:   team,
-				client: org.GithubClient(),
-			}
-			ghRepos = append(ghRepos, repo)
-
-			err := repo.Process()
-			if err != nil {
-				log.Printf("error populating repo '%s' from github: %s", repo.Details.Name, err.Error())
-			}
+			return nil, err
 		}
 
 		// Iterate over ghRepos and add the unarchived ones that have at least one commit.
-		for _, r := range ghRepos {
+		for _, r := range teamRepos {
 			if len(r.Details.Releases) > 0 || len(r.Details.Commits) > 0 {
-				out = append(out, r.Details)
+				orgRepos = append(orgRepos, r.Details)
 			}
 		}
 	}
 
-	return out, nil
+	return orgRepos, nil
+}
+
+// getTeamRepos fetches a team's repos from the Github client and processes them into
+// the format releasegen needs.
+func getTeamRepos(org OrgConfig, team string, orgRepos []repos.RepoDetails) ([]*Repository, error) {
+	ghRepos := []*Repository{}
+	ctx := context.Background()
+	opts := &gh.ListOptions{PerPage: githubPerPage}
+
+	// Lists the Github repositories that the 'ghTeam' has access to.
+	teamRepos, _, err := org.GithubClient().Teams.ListTeamReposBySlug(ctx, org.Org, team, opts)
+	if err != nil {
+		return nil, fmt.Errorf("error listing repositories for github org: %s", org.Org)
+	}
+
+	// Iterate over repositories, populating release info for each.
+	for _, tRepo := range teamRepos {
+		r := tRepo
+		// Check if the name of the repository is in the ignore list or private, or already processed.
+		if slices.Contains(org.IgnoredRepos, *r.Name) || *r.Private || repoInSlice(orgRepos, *r.Name) {
+			continue
+		}
+
+		repo := &Repository{
+			Details: repos.RepoDetails{
+				Name:          *r.Name,
+				DefaultBranch: *r.DefaultBranch,
+				URL:           *r.HTMLURL,
+			},
+			org:    org.Org,
+			team:   team,
+			client: org.GithubClient(),
+		}
+		ghRepos = append(ghRepos, repo)
+
+		err := repo.Process()
+		if err != nil {
+			log.Printf("error populating repo '%s' from github: %s", repo.Details.Name, err.Error())
+		}
+	}
+
+	return ghRepos, nil
+}
+
+// repoInSlice is a helper function to test if a given repo is already in a list of repos.
+func repoInSlice(repositories []repos.RepoDetails, r string) bool {
+	index := slices.IndexFunc(repositories, func(repo repos.RepoDetails) bool {
+		return repo.Name == r
+	})
+
+	return index >= 0
 }
